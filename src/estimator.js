756fed5fe619b195de0433281cb5aa4fd9625155
Sesinam <sesinam.sesig29@gmail.com>
2020-04-09 17:33:35 +0000

    Please enter the commit message for your changes. Lines starting "estimator build complete"

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,17 @@
-const covid19ImpactEstimator = (data) => data;
+/* eslint-disable linebreak-style */
+/* eslint-disable no-const-assign */
+
+import { input, impact, severeImpact } from './input';
+
+
+const covid19ImpactEstimator = (data) => {
+  const inputData = data;
+  inputData = input;
+  return {
+    data: inputData,
+    impact,
+    severeImpact
+  };
+};
 
 export default covid19ImpactEstimator;
